ae2a39e4b9e28786fc586eb8c8958d5008139d32
LoganLieou <logan.lieou@gmail.com>
2022-06-29 14:58:21 -0500

    first

---
+++ dist/cjs/types/components/BarChart/BarChart.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export default function BarChart(): JSX.Element;
---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+declare function BarChart(): JSX.Element;
+
+export { BarChart };
